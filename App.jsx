b702f770e45aca5181e6fcfb88cdabe05b01319e
Ayan Khan <beaayank@gmail.com>
2023-04-30 23:59:15 +0530

    Initial Commit

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// import React from "react"
+import Header from "./src/assets/Header"
+import Meme from "./src/assets/Meme"
+// import './App.css'
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
